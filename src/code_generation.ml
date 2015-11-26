15f8c8f996828c0e7a29317dc18d656671d1fdcb
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2015-11-26 16:51:30 +0100

    Use %a and fprintf in code generation

--- src/code_generation.ml
+++ src/code_generation.ml
@@ -1,24 +1,21 @@
-open Ident
 open Asttypes
 open Aez_ast
-
-let spf = Format.sprintf
-
-let str_ident ident = spf "%s__%i" ident.name ident.id
+open Format
 
 let str_base_type = function
   | Tbool -> "Type.type_bool"
   | Tint -> "Type.type_int"
   | Treal -> "Type.type_float"
 
-let generate_declare_symbol push decl =
-  let s_id = str_ident decl.sd_ident in
-  push (spf "let %s = declare_symbol \"%s\" [ Type.type_int ] %s@." s_id s_id (str_base_type decl.sd_type))
+let pp_ident = Ident.print  
 
-let generate_declare_symbols push =
-  List.iter (generate_declare_symbol push)
-  
-  
+let generate_declare_symbol ff decl =
+  fprintf ff "let %a = declare_symbol \"%a\" [ Type.type_int ] %s@." pp_ident decl.sd_ident pp_ident decl.sd_ident (str_base_type decl.sd_type)
+
+let generate_declare_symbols ff =
+  List.iter (generate_declare_symbol ff)
+
 let main decls out_id =
-  generate_declare_symbols (Format.printf "%s") decls;
-  Format.printf "@."
+  let ff = std_formatter in
+  generate_declare_symbols ff decls;
+  Format.fprintf ff "@."
